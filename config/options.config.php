62fa22550c23c6fdb2fd7fd73598605b42951751
cristian <cristian@192.168.90.139>
2017-03-15 12:14:09 -0300

    Initial Commit

--- config/options.config.php
+++ config/options.config.php
@@ -5,8 +5,8 @@ namespace ZfMetal\Commons;
 
 return[
     'zf-metal-commons.options' => [
-        'form_columns' => \ZfMetal\Commons\Constant\Form::ONE_COLUMNS, //Options: ONE_COLUMNS, TWO_COLUMNS, THREE_COLUMNS, FOUR_COLUMNS
-        'form_stype' => \ZfMetal\Commons\Constant\Form::VERTICAL, //Options: VERTICAL, HORIZONTAL, INLINE, PLACEHOLDER
+        'form_columns' => \ZfMetal\Commons\Consts::ONE_COLUMNS, //Options: ONE_COLUMNS, TWO_COLUMNS, THREE_COLUMNS, FOUR_COLUMNS
+        'form_stype' => \ZfMetal\Commons\Consts::VERTICAL, //Options: VERTICAL, HORIZONTAL, INLINE, PLACEHOLDER
         
     ]
 ];
