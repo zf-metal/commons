2480d6a61deed73a4189dbd43624582454224941
cristian <cristian@192.168.90.139>
2017-04-10 16:47:21 -0300

    Improve Forms

--- src/Consts.php
+++ src/Consts.php
@@ -9,14 +9,15 @@ namespace ZfMetal\Commons;
  */
 class Consts {
 
-    
     //FORMS
     const COLUMNS_ONE = "one";
     const COLUMNS_TWO = "two";
     const COLUMNS_THREE = "three";
     const COLUMNS_FOUR = "four";
     const STYLE_VERTICAL = "vertical";
+    const STYLE_VERTICAL_LG = "vertical-lg";
     const STYLE_HORIZONTAL = "horizontal";
+    const STYLE_HORIZONTAL_LG = "horizontal-lg";
     const STYLE_INLINE = "inline";
     const STYLE_PLACEHOLDER = "placeholder";
     const COLUMNS = [
@@ -31,12 +32,9 @@ class Consts {
         self::STYLE_INLINE => self::STYLE_INLINE,
         self::STYLE_PLACEHOLDER => self::STYLE_PLACEHOLDER,
     ];
-    
-    
     //MENU
     const STYLE_MENU_VERTICAL = "vertical";
     const STYLE_MENU_HORIZONTAL = "horizontal";
-    
     const STYLES_MENU = [
         self::STYLE_MENU_HORIZONTAL => self::STYLE_MENU_HORIZONTAL,
         self::STYLE_MENU_VERTICAL => self::STYLE_MENU_VERTICAL
--- src/Helper/View/RenderForm.php
+++ src/Helper/View/RenderForm.php
@@ -65,9 +65,11 @@ class RenderForm extends AbstractHelper {
     protected function classStyle() {
         switch ($this->style) {
             case \ZfMetal\Commons\Consts::STYLE_VERTICAL:
+            case \ZfMetal\Commons\Consts::STYLE_VERTICAL_LG:
                 $this->form->setAttribute("class", "form-vertical");
                 break;
             case \ZfMetal\Commons\Consts::STYLE_HORIZONTAL:
+            case \ZfMetal\Commons\Consts::STYLE_HORIZONTAL - lg:
                 $this->form->setAttribute("class", "form-horizontal");
                 break;
             case \ZfMetal\Commons\Consts::STYLE_INLINE:
