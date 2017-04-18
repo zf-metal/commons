512b393ef6cc165f2cd885d67bcc3185e249bbf8
cristian <cristian@192.168.90.139>
2017-04-18 09:42:35 -0300

    Ligero cambio

--- src/Helper/View/JQSelectPopulate.php
+++ src/Helper/View/JQSelectPopulate.php
@@ -7,21 +7,21 @@ use Zend\View\Helper\AbstractHelper;
 class JQSelectPopulate extends AbstractHelper {
 
     public function __invoke($url, $origin, $destination) {
-
-        $body = 'function populate_' . $destination . '(){' . PHP_EOL;
+        $body = "<script type='text/javascript'>";
+        $body .= 'function populate_' . $destination . '(){' . PHP_EOL;
 
         $body .= '$.getJSON( "' . $url . '", {id: $("[name  =\'' . $origin . '\']").val()} ,function( data ) {
-         var options = $("[name  =\'' . $destination . '\']");
-         options.find(\'option\').remove().end()
+         var comboBox = $("[name  =\'' . $destination . '\']");
+         comboBox.find(\'option\').remove().end();
          $.each( data, function(key,val) {
-             options.append(new Option(val,key));
+             comboBox.append(new Option(val,key));
          });
      });' . PHP_EOL;
 
         $body .= '}' . PHP_EOL;
 
         $body .= '$("[name  =\'' . $origin . '\']").change(populate_' . $destination . ');';
-
+        $body .= "</script>";
 
         return $body;
     }
