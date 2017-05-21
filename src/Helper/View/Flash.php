0cd1928574a738b51a7edb55ad66838ea5a716b9
cristian <cristian@192.168.5.112>
2017-05-21 17:30:28 -0300

    Fix Info Flash

--- src/Helper/View/Flash.php
+++ src/Helper/View/Flash.php
@@ -7,11 +7,11 @@ use Zend\View\Helper\AbstractHelper;
 class Flash extends AbstractHelper {
 
     public function __invoke() {
-            echo $this->view->flashMessenger()->render('success', array('alert', 'alert-success'));
-            echo $this->view->flashMessenger()->render('warning', array('alert', 'alert-warning'));
-            echo $this->view->flashMessenger()->render('default', array('alert', 'alert-info'));
-            echo $this->view->flashMessenger()->render('error', array('alert', 'alert-danger'));
-        
+        echo $this->view->flashMessenger()->render('success', array('alert', 'alert-success'));
+        echo $this->view->flashMessenger()->render('warning', array('alert', 'alert-warning'));
+        echo $this->view->flashMessenger()->render('default', array('alert', 'alert-info'));
+        echo $this->view->flashMessenger()->render('error', array('alert', 'alert-danger'));
+        echo $this->view->flashMessenger()->render('info', array('alert', 'alert-info'));
     }
 
 }
--- src/Helper/View/FlashCurrent.php
+++ src/Helper/View/FlashCurrent.php
@@ -7,14 +7,16 @@ use Zend\View\Helper\AbstractHelper;
 class FlashCurrent extends AbstractHelper {
 
     public function __invoke() {
-            echo $this->view->flashMessenger()->renderCurrent('success', array('alert', 'alert-success'));
-            echo $this->view->flashMessenger()->renderCurrent('warning', array('alert', 'alert-warning'));
-            echo $this->view->flashMessenger()->renderCurrent('default', array('alert', 'alert-info'));
-            echo $this->view->flashMessenger()->renderCurrent('error', array('alert', 'alert-danger'));
-            $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('default');
-            $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('success');
-            $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('warning');
-            $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('error');
+        echo $this->view->flashMessenger()->renderCurrent('success', array('alert', 'alert-success'));
+        echo $this->view->flashMessenger()->renderCurrent('warning', array('alert', 'alert-warning'));
+        echo $this->view->flashMessenger()->renderCurrent('default', array('alert', 'alert-info'));
+        echo $this->view->flashMessenger()->renderCurrent('error', array('alert', 'alert-danger'));
+        echo $this->view->flashMessenger()->renderCurrent('info', array('alert', 'alert-info'));
+        $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('info');
+        $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('default');
+        $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('success');
+        $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('warning');
+        $this->view->flashMessenger()->getPluginFlashMessenger()->clearCurrentMessagesFromNamespace('error');
     }
 
 }
