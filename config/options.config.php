1f159010ac0147d3c90af73335040d2cff7b2ff1
cristian <cristian@192.168.5.112>
2017-05-23 18:22:37 -0300

    Init Export

--- config/options.config.php
+++ config/options.config.php
@@ -2,13 +2,18 @@
 
 namespace ZfMetal\Commons;
 
-
 return[
     'zf-metal-commons.options' => [
         'form_columns' => \ZfMetal\Commons\Consts::COLUMNS_ONE, //Options: ONE_COLUMNS, TWO_COLUMNS, THREE_COLUMNS, FOUR_COLUMNS
         'form_style' => \ZfMetal\Commons\Consts::STYLE_VERTICAL, //Options: VERTICAL, HORIZONTAL, INLINE, PLACEHOLDER
         'form_process_msj_ok' => "Registro guardado",
         'form_process_msj_fail' => "Datos invalidos o incompletos. Por favor revisar formulario.",
+    ],
+    'zf-metal-commons.exports' => [
+        'config-key-1' => [
+        ],
+        'config-key-2' => [
+        ],
     ]
 ];
 
---
+++ src/Options/ExcelExportConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ZfMetal\Commons\Options;
+
+use Zend\Stdlib\AbstractOptions;
+
+class ExcelExportConfig extends AbstractOptions {
+
+    protected $entity;
+    /**
+     *
+     * @var array
+     */
+    protected $fields;
+
+}
