a242e230b0483be7ce2d9c21c4b2caef53aac76a
cristian.cdi@gmail.com <Neverwinter2>
2019-01-28 23:36:34 -0300

    Add NoObjectExist and ObjectExist Validators Factory

--- config/form.config.php
+++ config/form.config.php
@@ -15,6 +15,8 @@ return [
     'validators' => [
         'factories' => [
             'UniqueObject' => \ZfMetal\Commons\Factory\Validator\UniqueObjectFactory::class,
+            'ObjectExists' => \ZfMetal\Commons\Factory\Validator\ObjectExistsFactory::class,
+            'NoObjectExists' => \ZfMetal\Commons\Factory\Validator\NoObjectExistsFactory::class,
         ],
 
     ],
