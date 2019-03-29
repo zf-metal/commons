52e7d1035e2a1899ef880681362c7ac1a42cb5aa
cristian.cdi@gmail.com <Neverwinter2>
2019-03-29 19:46:42 -0300

    Se agregan validadores

--- config/form.config.php
+++ config/form.config.php
@@ -9,6 +9,8 @@ return [
     'validators' => [
         'factories' => [
             'UniqueObject' => \ZfMetal\Commons\Factory\Validator\UniqueObjectFactory::class,
+            'ObjectExists' => \ZfMetal\Commons\Factory\Validator\ObjectExistsFactory::class,
+            'NoObjectExists' => \ZfMetal\Commons\Factory\Validator\NoObjectExistsFactory::class,
         ],
 
     ],
